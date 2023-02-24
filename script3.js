2a0377bfc5c4ccc06e72d345e800d126ecdc894a
freskozi <denisbandic98@gmail.com>
2023-02-24 19:08:13 +0100

    aa

---
+++ script3.js
@@ -0,0 +1,24 @@
+//const { _ } = require("core-js");
+// 1.
+var dobro = 1234567890;
+var lose = "1123456";
+var jedinstveniZnakovi = function(password) {
+    // vas kod
+    console.log("test " + password)
+    let isNum = _.isNumber(password);
+    console.log("is number " + isNum);
+};
+
+function isUniqueCharacters(password)
+{
+    for(var i = 0; i <= password.length; i++){
+        
+    }
+}
+
+function noDups( s ) {
+
+}
+
+jedinstveniZnakovi(dobro);
+jedinstveniZnakovi(lose);
