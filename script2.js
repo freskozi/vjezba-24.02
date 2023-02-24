bfdf35e3235734ad903ce1e7daca4b37499a3eb0
freskozi <denisbandic98@gmail.com>
2023-02-24 18:17:18 +0100

    aaaa

---
+++ script2.js
@@ -0,0 +1,8 @@
+var d = new Date();
+console.log(d);
+var d1 = new Date(2018, 11, 24, 10, 33, 30, 0);
+console.log(d1);
+var d2 = new Date("October 13, 2014 11:13:00");
+var d3 = new Date(-10000000000)
+console.log(d2);
+console.log(d3);
