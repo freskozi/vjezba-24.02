c08f67718bfe9d0e88117caf270d5b77af7d910c
freskozi <denisbandic98@gmail.com>
2023-02-28 19:14:21 +0100

    fdsgfd

---
+++ let i var.js
@@ -0,0 +1,15 @@
+{
+    let x = 2;
+    //console.log (x)
+}
+
+console.log(x)
+
+var y = 3;
+console.log(y)
+
+{
+    var y = 3;
+    console.log(y)
+    
+}
---
+++ zbrajanje.js
@@ -0,0 +1,9 @@
+var zbroj = 0;
+for(i=1; i<7; i++)
+{if(i%2==0)
+{
+    zbroj += i;
+}
+
+}
+console.log(zbroj);
